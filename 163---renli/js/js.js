54fd1b31fe2b3e39785efbaeb18804794830299b
GuangYao.Cao <caoguangyao28@163.com>
2018-12-07 18:01:01 +0800

    1 修正

--- 163---renli/js/js.js
+++ 163---renli/js/js.js
@@ -1,19 +1,22 @@
-$(function(){
-    var index=0;   
-    var pos=$('.slide-inner li');
-$('.slide-right').click(right);
-function right(){
-      index++;
-      if(index == pos.length){
-          index=0;
-      }
-      pos.eq(index).fadeIn().siblings().fadeOut();
-  }
-$('.slide-left').click(left);
-  function left(){
-       index--; 
-        if(index < 0){
-            index=pos.length-1;
+$(function () {
+    var index = 0;
+    var pos = $('.slide-inner li');
+    $('.slide-right').click(right);
+
+    function right() {
+        index++;
+        if (index == pos.length) {
+            index = 0;
+        }
+        pos.eq(index).fadeIn().siblings().fadeOut();
+    }
+
+    $('.slide-left').click(left);
+
+    function left() {
+        index--;
+        if (index < 0) {
+            index = pos.length - 1;
         }
         pos.eq(index).fadeIn().siblings().fadeOut();
     }
